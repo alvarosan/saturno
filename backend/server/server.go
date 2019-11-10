9389f5fac72cc4071e31789a41ccf0a7abe2aff6
Alvaro Sanchez <a.sanchez@smartreporting.de>
2019-11-10 13:29:48 +0100

    Copying image array elementwise
    This is a first (non-efficient) implementation of the image
    wrapper of the rust array. Added an C accessor for element values
    to simplify pointer arithmetic (although pointer arithmetic is not
    natively supported in Go, it is possible to do it through
    unsafe.Pointer; however the syntax becomes terribly verbose).
    
    Image is still missaligned. It is still necessary to figure out
    the ownership of the underlying memory once rust returns the const
    pointer (?).

--- backend/server/server.go
+++ backend/server/server.go
@@ -1,58 +1,88 @@
 package server
 
+/**
+ * C acessor for the array (it is possible to access directly in Go
+ * with pointer arithmetic through unsafe.Pointer).
+ *
+ * https://stackoverflow.com/questions/49987098/how-to-access-a-c-pointer-array-from-golang
+ */
+
 // #cgo LDFLAGS: -L /home/alvaro/workspace/source/saturno/rendering/target/release  -lrendering_c_abi
+//
 // extern unsigned char* get_frame();
+//
+// unsigned char get_value(void* data, int index) {
+//     unsigned char* data_uchar = (unsigned char*) data;
+//     return data_uchar[index];
+// }
 import "C"
 
 import (
-	"fmt"
-	"image"
-	"image/png"
-	"log"
-	"net/http"
-	"os"
+    "fmt"
+    "image"
+    "image/png"
+    "image/color"
+    "log"
+    "net/http"
+    "os"
+    "unsafe"
 )
 
 var LISTENING_PORT = os.Getenv("LISTENING_PORT")
 
 func Initialize() {
-	http.HandleFunc("/clientside", handleClientSideApp)
-	http.HandleFunc("/serverside", handleServerSideApp)
-	fmt.Println("> Server initialized, listening on port " + LISTENING_PORT)
+    http.HandleFunc("/clientside", handleClientSideApp)
+    http.HandleFunc("/serverside", handleServerSideApp)
+    fmt.Println("> Server initialized, listening on port " + LISTENING_PORT)
 
-	log.Fatal(http.ListenAndServe(":"+LISTENING_PORT, nil))
+    log.Fatal(http.ListenAndServe(":"+LISTENING_PORT, nil))
 }
 
 func handleClientSideApp(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("> Serving clientside-rendering app ... ")
-	fmt.Fprintf(w, "PONG! /clientside API '%v'", r.Method)
+    fmt.Println("> Serving clientside-rendering app ... ")
+    fmt.Fprintf(w, "PONG! /clientside API '%v'", r.Method)
 }
 
 func handleServerSideApp(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("> Serving serverside-rendering app ... ")
-	//fmt.Fprintf(w, "PONG! /serverside API '%v' '%d'", r.Method, result)
+    fmt.Println("> Serving serverside-rendering app ... ")
+    //fmt.Fprintf(w, "PONG! /serverside API '%v' '%d'", r.Method, result)
 }
 
 func WrapImage() {
-	//var result = C.get_frame()
+    var height int = 100
+    var width int = 200
+    myImage := image.NewNRGBA(image.Rect(0, 0, width, height))
+    var frame_ptr = unsafe.Pointer(C.get_frame())
 
-	myImage := image.NewNRGBA(image.Rect(0, 0, 200, 100))
+    for y := 0; y < height; y++ {
+        for x := 0; x < width; x++ {
+            red := uint8(C.get_value(frame_ptr, C.int(3 * ( y * width + x ))))
+            green := uint8(C.get_value(frame_ptr, C.int(3 * ( y * width + x ) + 1)))
+            blue := uint8(C.get_value(frame_ptr, C.int(3 * ( y * width + x ) + 2)))
+            alpha := uint8(C.get_value(frame_ptr, C.int(3 * ( y * width + x ) + 3)))
+            myImage.SetNRGBA(x, y, color.NRGBA{red, green, blue, alpha})
+        }
+    }
 
-	// outputFile is a File type which satisfies Writer interface
-	outputFile, err := os.Create("my_test.png")
-        checkErr(err)
+    writeImageToFile(myImage)
+}
 
-	// Encode takes a writer interface and an image interface
-	// We pass it the File and the RGBA
-	png.Encode(outputFile, myImage)
+func writeImageToFile(img image.Image) {
+    // outputFile is a File type which satisfies Writer interface
+    outputFile, err := os.Create("my_test.png")
+    checkErr(err)
 
-	// Don't forget to close files
-	outputFile.Close()
+    // Encode takes a writer interface and an image interface
+    // We pass it the File and the RGBA
+    png.Encode(outputFile, img)
+
+    // Don't forget to close files
+    outputFile.Close()
 }
 
 func checkErr(err error) {
-	if err != nil {
-		fmt.Println(">! Panicked over: ", err.Error())
-		panic(err)
-	}
+    if err != nil {
+        fmt.Println(">! Panicked over: ", err.Error())
+        panic(err)
+    }
 }
