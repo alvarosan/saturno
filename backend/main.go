be95056c04864569b013547c996ee15c28c7bb60
Alvaro Sanchez <a.sanchez@smartreporting.de>
2019-11-10 13:14:00 +0100

    Adding image wrapper in go.

--- backend/main.go
+++ backend/main.go
@@ -4,7 +4,8 @@ import "./server"
 
 func main() {
 
-        server.Initialize()
+        //server.Initialize()
+        server.WrapImage()
 
 	// TODO linking Rust into Go
 	// https://blog.filippo.io/rustgo/
--- backend/server/server.go
+++ backend/server/server.go
@@ -1,11 +1,13 @@
 package server
 
 // #cgo LDFLAGS: -L /home/alvaro/workspace/source/saturno/rendering/target/release  -lrendering_c_abi
-// extern void* get_frame();
+// extern unsigned char* get_frame();
 import "C"
 
 import (
 	"fmt"
+	"image"
+	"image/png"
 	"log"
 	"net/http"
 	"os"
@@ -18,7 +20,7 @@ func Initialize() {
 	http.HandleFunc("/serverside", handleServerSideApp)
 	fmt.Println("> Server initialized, listening on port " + LISTENING_PORT)
 
-	log.Fatal(http.ListenAndServe(":" + LISTENING_PORT, nil))
+	log.Fatal(http.ListenAndServe(":"+LISTENING_PORT, nil))
 }
 
 func handleClientSideApp(w http.ResponseWriter, r *http.Request) {
@@ -28,10 +30,24 @@ func handleClientSideApp(w http.ResponseWriter, r *http.Request) {
 
 func handleServerSideApp(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("> Serving serverside-rendering app ... ")
+	//fmt.Fprintf(w, "PONG! /serverside API '%v' '%d'", r.Method, result)
+}
 
-        var result = C.get_frame();
-	fmt.Fprintf(w, "PONG! /serverside API '%v' '%d'", r.Method, result)
+func WrapImage() {
+	//var result = C.get_frame()
 
+	myImage := image.NewNRGBA(image.Rect(0, 0, 200, 100))
+
+	// outputFile is a File type which satisfies Writer interface
+	outputFile, err := os.Create("my_test.png")
+        checkErr(err)
+
+	// Encode takes a writer interface and an image interface
+	// We pass it the File and the RGBA
+	png.Encode(outputFile, myImage)
+
+	// Don't forget to close files
+	outputFile.Close()
 }
 
 func checkErr(err error) {
