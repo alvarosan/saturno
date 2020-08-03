827fb66e560400114a35560110ad114c33754391
Alvaro Sanchez <asmoscosa@gmail.com>
2020-08-03 22:55:43 +0200

    Render frame server-side.

--- server/src/main.rs
+++ server/src/main.rs
@@ -5,8 +5,16 @@ extern crate rendering;
 #[macro_use]
 extern crate rocket;
 
+extern crate image;
+
+use image::ColorType;
+use image::png::PNGEncoder;
 use rendering::raytracer::external;
 use rendering::raytracer::canvas::Canvas;
+
+use rocket::http::ContentType;
+use rocket::response::Response;
+use rocket::response::content::Content;
 
 // TODO
 //#[get("/api/v1/render")]
@@ -22,12 +30,30 @@ pub fn create_renderer(scene_id: u32) -> Renderer {
     }
 }
 
-#[get("/")]
-fn index() -> &'static str {
+#[get("/v1/render")]
+//fn get_frame() -> &'static str {
+fn get_frame() -> Content<Vec<u8>> {
     let renderer = create_renderer(0);
-    "Created renderer!"
+    let image = renderer.canvas.render_scene();
+
+    let mut buffer: Vec<u8> = Vec::new();
+    let myimage: Vec<u8> = image.data;
+    let imagergba = image::RgbaImage::from_raw(image.width, image.height, myimage.clone());
+    let image_png = image::DynamicImage::ImageRgba8(imagergba.unwrap());
+
+    let _result = image_png.write_to(&mut buffer,image::ImageOutputFormat::PNG);
+
+    Content(ContentType::PNG, buffer)
+
+    //response
+//    "Created renderer!"
+}
+
+#[get("/health")]
+fn health() -> &'static str {
+    "Ok"
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/api", routes![health, get_frame]).launch();
 }
